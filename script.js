b841c8eb2fc41211cfd504efb5c60c6117518417
iqrahy <iqrahyderxwave@gmail.com>
2024-08-26 12:14:11 +0500

    tab assignment

---
+++ script.js
@@ -0,0 +1,20 @@
+let tabs = document.querySelectorAll(".tab-btn");
+let content = document.querySelectorAll(".content");
+
+tabs.forEach((tab, index) => {
+  tab.addEventListener("click", (e) => {
+    tabs.forEach((tab) => {
+      tab.classList.remove("active");
+    });
+    tab.classList.add("active");
+
+    let line = document.querySelector(".line");
+    line.style.width = e.target.offsetWidth + "px";
+    line.style.left = e.target.offsetLeft + "px";
+
+    content.forEach(a=>{
+        a.classList.remove('active');
+    })
+    content[index].classList.add('active');
+  });
+});
